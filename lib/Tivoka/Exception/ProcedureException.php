a3cbcd3312259a2261dbf133b9f989bc170a148b
Kirill Glazunov <reg@webmutant.ru>
2013-02-20 16:41:38 +0400

    Add method for getting exception data

--- lib/Tivoka/Exception/ProcedureException.php
+++ lib/Tivoka/Exception/ProcedureException.php
@@ -34,5 +34,29 @@ namespace Tivoka\Exception;
  * Tivoka ProcedureException
  * @package Tivoka
  */
-class ProcedureException extends Exception {}
+class ProcedureException extends Exception {
+
+	/**
+	 * @var mixed $data is additional variable which will sent to client with exception.
+	 */
+	private $data;
+
+	/**
+	 * @param string    $message  A String providing a short description of the error.
+	 *                            The message SHOULD be limited to a concise single sentence.
+	 * @param int       $code     A Number that indicates the error type that occurred.
+	 * @param Exception $previous
+	 * @param mixed     $data     A Primitive or Structured value that contains additional information about the error.
+	 */
+	public function __construct($message, $code, Exception $previous = null, $data = array()) {
+		$this->data = $data;
+
+		parent::__construct($message, $code, $previous);
+	}
+
+
+	public function getData() {
+		return $this->data;
+	}
+}
 ?>
